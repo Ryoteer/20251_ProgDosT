8d3e541dadca3aab2e0ead057a30d3de5f281ea5
Ryoteer <72623439+Ryoteer@users.noreply.github.com>
2025-04-09 17:49:01 -0300

    Clase 3
    Animaciones, parte 1.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerBehaviour : MonoBehaviour
 {
+    [Header("Animator")]
+    [SerializeField] private string _airBoolName = "isOnAir";
+    [SerializeField] private string _jumpTriggerName = "onJump";
+    [SerializeField] private string _xAxisName = "xAxis";
+    [SerializeField] private string _zAxisName = "zAxis";
+
     [Header("Inputs")]
     [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
 
@@ -11,8 +18,11 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField] private float _jumpForce = 5.0f;
     [SerializeField] private float _moveSpeed = 3.5f;
 
+    private bool _isOnAir = false;
+
     private Vector3 _dir;
 
+    private Animator _animator;
     private Rigidbody _rb;
 
     private void Awake()
@@ -20,13 +30,21 @@ public class PlayerBehaviour : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
     private void Update()
     {
         _dir.x = Input.GetAxis("Horizontal");
+        _animator.SetFloat(_xAxisName, _dir.x);
         _dir.z = Input.GetAxis("Vertical");
+        _animator.SetFloat(_zAxisName, _dir.z);
 
-        if (Input.GetKeyDown(_jumpKey))
+        if (Input.GetKeyDown(_jumpKey) && !_isOnAir)
         {
+            _animator.SetTrigger(_jumpTriggerName);
             Jump();
         }
     }
@@ -47,5 +65,23 @@ public class PlayerBehaviour : MonoBehaviour
     private void Movement(Vector3 dir)
     {
         _rb.MovePosition(transform.position + dir.normalized * _moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 30)
+        {
+            _isOnAir = false;
+            _animator.SetBool(_airBoolName, _isOnAir);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.layer == 30)
+        {
+            _isOnAir = true;
+            _animator.SetBool(_airBoolName, _isOnAir);
+        }
     }
 }
